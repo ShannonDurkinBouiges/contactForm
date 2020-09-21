9a329de0935f25d45b4bb5c64da5015b4e138ede
Shannon Durkin-Bouiges <ShannonDurkinBouiges@gmail.com>
2020-09-21 10:40:05 +0200

    Created form layout

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Contact Form</title>
+    </head>
+    <body>
+        <main>
+            <p>Send E-mail</p>
+            <form class="contactForm" action="contactForm.php" method="POST">
+                <input type="text" name="name" placeholder="Nom">
+                <input type="text" name="email" placeholder="Votre e-mail">
+                <input type="text" name="sujet" placeholder="Sujet">
+                <textarea name="message" placeholder="Message"></textarea>
+                <button type="submit" name="submit">Envoyer</button>
+            </form>
+        </main>
+    </body>
+</html>
+
