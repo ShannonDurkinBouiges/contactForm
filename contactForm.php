b357d52dc2f4a4edb9c0778313d1276895c78b87
Shannon Durkin-Bouiges <ShannonDurkinBouiges@gmail.com>
2020-09-21 11:04:55 +0200

    Added mail and header functions

--- contactForm.php
+++ contactForm.php
@@ -5,5 +5,12 @@ if (isset($_POST['submit'])) {
     $subject = $_POST['subject'];
     $mailFrom = $_POST['email'];
     $message = $_POST['message'];
+    
+    $mailTo = "Shannon_Durkin@ymail.com";
+    $headers = "From: ".$mailFrom;
+    $txt = "Vous avez reçu un e-mail de ".$name.".\n\n".$message;
+    
+    mail($mailTo, $subject, $txt, $headers);
+    header("Location: index.php?mailsend");
 }
 
